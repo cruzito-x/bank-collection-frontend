d98793b67fc1367ca65d648459418f873e834f1b
David Cruz <dcruzer92@gmail.com>
2025-01-11 01:07:44 -0600

    Login has been upgraded and antd lib has been added

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,45 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, Card, Form, Image, Input } from "antd";
+import { EyeInvisibleOutlined, EyeTwoTone } from "@ant-design/icons";
 import Logo from "../../assets/img/logo.png";
-import './styles/login.css';
+import "./styles/login.css";
 
 const Login = () => {
+  const [loading, setLoading] = useState(false);
+
   return (
-    <section className="mt-5 pt-5">
-      <div className="container pt-5 mt-5">
-        <div className="row">
-          <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12 m-auto">
-            <div className="card shadow text-center">
-              <div className="card-body">
-                <img src={Logo} className="shadow mb-4" alt="Logo del Sistema" />
-                <form>
-                  <div className="form-group text-start mb-3">
-                    <label className="font-weight-bold">Usuario</label>
-                    <input
-                      type="text"
-                      className="form-control"
-                      placeholder="Introduzca su Usuario"
-                    />
-                  </div>
-                  <div className="form-group text-start mb-3">
-                    <label className="font-weight-bold">Contraseña</label>
-                    <input
-                      type="password"
-                      className="form-control"
-                      placeholder="Introduzca su Contraseña"
-                    />
-                  </div>
-                  <div className="form-group text-center mb-3">
-                    <button type="submit" className="btn btn-primary btn-block">
-                      Iniciar Sesión
-                    </button>
-                  </div>
-                </form>
-              </div>
-            </div>
-          </div>
+    <div className="login-container">
+      <Card>
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "center",
+            marginBottom: "15px",
+          }}
+        >
+          <Image
+            src={Logo}
+            preview={false}
+            style={{ width: "75px", height: "75px", borderRadius: "50%" }}
+            alt="Logo del Sistema"
+          />
         </div>
-      </div>
-    </section>
+        <Form
+          name="loginForm"
+          layout={"vertical"}
+          initialValues={{ remember: true }}
+          style={{ width: 300 }}
+        >
+          <Form.Item
+            label="Usuario:"
+            name="username"
+            rules={[
+              {
+                message: "Por favor introduzca su usuario",
+              },
+            ]}
+          >
+            <Input placeholder="Usuario" />
+          </Form.Item>
+
+          <Form.Item
+            label="Contraseña:"
+            name="password"
+            rules={[{ message: "Por favor introduzca su contraseña" }]}
+          >
+            <Input.Password
+              placeholder="Contraseña"
+              iconRender={(visible) =>
+                visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
+              }
+            />
+          </Form.Item>
+
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              loading={loading}
+              style={{ width: "100%" }}
+            >
+              Iniciar Sesión
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
   );
 };
 
