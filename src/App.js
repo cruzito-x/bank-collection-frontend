8003b772ba361da4e9c78c5727212d1b49f17206
David Cruz <dcruzer92@gmail.com>
2025-01-11 01:09:16 -0600

    The login view is fixed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import Footer from "./components/footer/Footer";
 import Login from "./components/login/Login";
 
 function App() {
   return (
     <>
       <Login />
-      <Footer />
     </>
   );
 }
