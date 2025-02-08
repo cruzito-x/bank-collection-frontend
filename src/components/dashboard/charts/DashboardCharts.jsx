568b6e116a5a46a9423d1918858388e964f9c0da
David Cruz <dcruzer92@gmail.com>
2025-02-08 04:08:11 -0600

    2 charts has been added

--- src/components/dashboard/charts/DashboardCharts.jsx
+++ src/components/dashboard/charts/DashboardCharts.jsx
@@ -12,6 +12,11 @@ const DashboardCharts = ({
   const [transactionsByDenomination, setTransactionsByDenomination] = useState(
     []
   );
+  const [customersWithTheMostMoneyPaid, setCustomersWithTheMostMoneyPaid] =
+    useState([]);
+  const [approvalAndRejectionRates, setApprovalAndRejectionRates] = useState(
+    []
+  );
 
   const barTransactionsCanvasRef = useRef(null);
   const barTransactionsChartInstance = useRef(null);
@@ -21,6 +26,12 @@ const DashboardCharts = ({
 
   const doughnutAmountCanvasRef = useRef(null);
   const doughnutAmountChartInstance = useRef(null);
+
+  const barCustomersWhitMostMoneyPaidRef = useRef(null);
+  const barCustomersWhitMostMoneyPaidChartInstance = useRef(null);
+
+  const doughnutRatesCanvasRef = useRef(null);
+  const doughnutRatesChartInstance = useRef(null);
 
   const colors = [
     "#3e9bff", // Azul Intenso
@@ -85,6 +96,30 @@ const DashboardCharts = ({
     setTransactionsByDenomination(transactionsByDenominationData);
   };
 
+  const getCustomersWithTheMostMoneyPaid = async () => {
+    const response = await fetch(
+      "http://localhost:3001/dashboard/customers-with-the-most-money-paid",
+      {
+        method: "GET",
+      }
+    );
+
+    const customersWithTheMostMoneyPaidData = await response.json();
+    setCustomersWithTheMostMoneyPaid(customersWithTheMostMoneyPaidData);
+  };
+
+  const getApprovalAndRejectionRates = async () => {
+    const response = await fetch(
+      "http://localhost:3001/dashboard/approval-and-rejection-rates",
+      {
+        method: "GET",
+      }
+    );
+
+    const approvalAndRejectionRatesData = await response.json();
+    setApprovalAndRejectionRates(approvalAndRejectionRatesData);
+  };
+
   useEffect(() => {
     getTransactionsByDateAndAmountRangeAndType();
   }, [datesRange[0], datesRange[1], amountRangeFilter, transactionTypeFilter]);
@@ -92,6 +127,7 @@ const DashboardCharts = ({
   useEffect(() => {
     getTransactionsByCollector();
     getTransactionsByDenomination();
+    getApprovalAndRejectionRates();
   }, []);
 
   useEffect(() => {
@@ -178,7 +214,7 @@ const DashboardCharts = ({
       );
     }
 
-    // Transactions by Collector Chart
+    // Transactions by Collector
     if (transactionsByCollector.length > 0) {
       const collectors = transactionsByCollector.map(
         (transactionByCollector) => transactionByCollector.collector
@@ -297,6 +333,135 @@ const DashboardCharts = ({
       );
     }
 
+    // Customers With The Most Money Paid
+    if (customersWithTheMostMoneyPaid.length > 0) {
+      const customers = customersWithTheMostMoneyPaid.map(
+        (customer) => customer.customer
+      );
+
+      const amounts = customersWithTheMostMoneyPaid.map(
+        (customer) => customer.amount
+      );
+
+      const barCustomersWhitMostMoneyPaidChart =
+        doughnutTransactionsCanvasRef.current.getContext("2d");
+
+      const barbarCustomersWhitMostMoneyPaidData = {
+        labels: customers,
+        datasets: [
+          {
+            data: amounts,
+            backgroundColor: colors,
+          },
+        ],
+      };
+
+      const barCustomersWhitMostMoneyPaidConfig = {
+        type: "bar",
+        data: barbarCustomersWhitMostMoneyPaidData,
+        options: {
+          aspectRatio: 2,
+          maintainAspectRatio: true,
+          responsive: true,
+          plugins: {
+            title: {
+              display: true,
+              text: "Ranking - Top 5 Clientes con más Pagos",
+              color: "#000000",
+              margin: {
+                bottom: 20,
+              },
+            },
+            legend: {
+              position: "left",
+              labels: {
+                boxWidth: 20,
+                padding: 10,
+              },
+            },
+            tooltip: {
+              callbacks: {
+                label: function (total, data) {
+                  return "Cantidad: " + total.formattedValue;
+                },
+              },
+            },
+          },
+          borderWidth: 0,
+        },
+      };
+
+      barCustomersWhitMostMoneyPaidChartInstance.current = new Chart(
+        barCustomersWhitMostMoneyPaidChart,
+        barCustomersWhitMostMoneyPaidConfig
+      );
+    }
+
+    // Approval and Rejection Rates
+    if (approvalAndRejectionRates.length > 0) {
+      const approvals = approvalAndRejectionRates.map(
+        (rate) => rate.approved_transactions
+      );
+
+      const rejected = approvalAndRejectionRates.map(
+        (rate) => rate.rejected_transactions
+      );
+
+      const doughnutRatesChart =
+        doughnutRatesCanvasRef.current.getContext("2d");
+
+      const doughnutRatesData = {
+        labels: ["Aprobadas", "Rechazadas"],
+        datasets: [
+          {
+            data: [approvals, rejected],
+            backgroundColor: [colors[0], colors[4]],
+          },
+        ],
+      };
+
+      const doughnutRatesConfig = {
+        type: "bar",
+        data: doughnutRatesData,
+        options: {
+          indexAxis: "y",
+          aspectRatio: 2,
+          maintainAspectRatio: true,
+          responsive: true,
+          plugins: {
+            title: {
+              display: true,
+              text: "Tasa de Aprobaciones y Rechazos - Transacciones",
+              color: "#000000",
+              margin: {
+                bottom: 20,
+              },
+            },
+            legend: {
+              position: "left",
+              labels: {
+                boxWidth: 20,
+                padding: 10,
+              },
+            },
+            tooltip: {
+              callbacks: {
+                label: function (total, data) {
+                  return "Total: " + total.formattedValue;
+                },
+              },
+            },
+          },
+          borderWidth: 0,
+        },
+      };
+
+      doughnutRatesChartInstance.current = new Chart(
+        doughnutRatesChart,
+        doughnutRatesConfig
+      );
+    }
+
     return () => {
       if (barTransactionsChartInstance.current) {
         barTransactionsChartInstance.current.destroy();
@@ -309,65 +474,116 @@ const DashboardCharts = ({
       if (doughnutTransactionsChartInstance.current) {
         doughnutTransactionsChartInstance.current.destroy();
       }
+
+      if (barCustomersWhitMostMoneyPaidChartInstance.current) {
+        barCustomersWhitMostMoneyPaidChartInstance.current.destroy();
+      }
+
+      if (doughnutRatesChartInstance.current) {
+        doughnutRatesChartInstance.current.destroy();
+      }
     };
-  }, [transactionsByDate, transactionsByCollector, transactionsByDenomination]);
+  }, [
+    transactionsByDate,
+    transactionsByCollector,
+    transactionsByDenomination,
+    customersWithTheMostMoneyPaid,
+    approvalAndRejectionRates,
+  ]);
 
   return (
-    <div className="row">
-      <div className="col-xl-8 col-lg-8 col-md-8 col-sm-12 mb-1">
-        <Card
-          className={`mb-2 ${
-            transactionsByDate.length === 0
-              ? "d-flex justify-content-center align-items-center h-100"
-              : ""
-          }`}
-          bodyStyle={{ padding: 0 }}
-        >
-          {transactionsByDate.length === 0 ? (
-            <Empty />
-          ) : (
-            <canvas
-              className="ms-3 me-3 mb-4"
-              ref={barTransactionsCanvasRef}
-            ></canvas>
-          )}
-        </Card>
+    <>
+      <div className="row mb-2">
+        <div className="col-xl-8 col-lg-8 col-md-8 col-sm-12 mb-1">
+          <Card
+            className={`mb-2 ${
+              transactionsByDate.length === 0
+                ? "d-flex justify-content-center align-items-center h-100"
+                : ""
+            }`}
+            bodyStyle={{ padding: 0 }}
+          >
+            {transactionsByDate.length === 0 ? (
+              <Empty />
+            ) : (
+              <canvas
+                className="ms-3 me-3 mb-4"
+                ref={barTransactionsCanvasRef}
+              ></canvas>
+            )}
+          </Card>
+        </div>
+        <div className="col-xl-4 col-lg-4 col-md-4 col-sm-12">
+          <Card
+            className={`mb-2 ${
+              transactionsByCollector.length === 0
+                ? "d-flex justify-content-center align-items-center h-100"
+                : ""
+            }`}
+            bodyStyle={{ padding: 0 }}
+          >
+            {transactionsByCollector.length === 0 ? (
+              <Empty />
+            ) : (
+              <canvas className="mb-2" ref={doughnutAmountCanvasRef}></canvas>
+            )}
+          </Card>
+          <Card
+            className={`${transactionsByDenomination.length === 0 ? "" : ""}`}
+            bodyStyle={{ padding: 0 }}
+          >
+            {transactionsByDenomination.length === 0 ? (
+              <Empty />
+            ) : (
+              <Carousel arrows infinite={false} dotPosition="bottom">
+                <div>
+                  <canvas
+                    className="mb-2"
+                    ref={doughnutTransactionsCanvasRef}
+                  ></canvas>
+                </div>
+                <div></div>
+              </Carousel>
+            )}
+          </Card>
+        </div>
       </div>
-      <div className="col-xl-4 col-lg-4 col-md-4 col-sm-12">
-        <Card
-          className={`mb-2 ${
-            transactionsByCollector.length === 0
-              ? "d-flex justify-content-center align-items-center h-100"
-              : ""
-          }`}
-          bodyStyle={{ padding: 0 }}
-        >
-          {transactionsByCollector.length === 0 ? (
-            <Empty />
-          ) : (
-            <canvas className="mb-2" ref={doughnutAmountCanvasRef}></canvas>
-          )}
-        </Card>
-        <Card
-          className={`${transactionsByDenomination.length === 0 ? "" : ""}`}
-          bodyStyle={{ padding: 0 }}
-        >
-          {transactionsByDenomination.length === 0 ? (
-            <Empty />
-          ) : (
-            <Carousel arrows infinite={false} dotPosition="bottom">
+      <div className="row">
+        <div className="col-xl-7">
+          <Card
+            className={`${
+              customersWithTheMostMoneyPaid.length === 0 ? "" : ""
+            }`}
+            bodyStyle={{ padding: 0 }}
+          >
+            {customersWithTheMostMoneyPaid.length === 0 ? (
+              <Empty />
+            ) : (
               <div>
                 <canvas
                   className="mb-2"
-                  ref={doughnutTransactionsCanvasRef}
+                  ref={barCustomersWhitMostMoneyPaidRef}
                 ></canvas>
               </div>
-              <div></div>
-            </Carousel>
-          )}
-        </Card>
+            )}
+          </Card>
+        </div>
+        <div className="col-xl-5">
+          <Card
+            className={`${approvalAndRejectionRates.length === 0 ? "" : ""}`}
+            bodyStyle={{ padding: 0 }}
+          >
+            {approvalAndRejectionRates.length === 0 ? (
+              <Empty />
+            ) : (
+              <div>
+                <canvas className="mb-2" ref={doughnutRatesCanvasRef}></canvas>
+              </div>
+            )}
+          </Card>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
