65b6dcc7e7f3571dbce0f5b8233db3374621ed09
David Cruz <dcruzer92@gmail.com>
2025-02-11 20:16:40 -0600

    404 fixed

--- src/utils/notFound/NotFound.jsx
+++ src/utils/notFound/NotFound.jsx
@@ -1,41 +1,58 @@
 import React from "react";
 import { Button, Result } from "antd";
 import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../contexts/authContext/AuthContext";
 
 const NotFound = () => {
   const navigate = useNavigate();
+  const { authState } = useAuth();
+  const token = authState.token;
+  const isSupervisor = authState.isSupervisor;
 
   const goToHome = () => {
-    navigate("/dashboard");
+    if (token) {
+      navigate(isSupervisor ? "/dashboard" : "/customers");
+    } else {
+      navigate("/");
+    }
   };
 
   return (
     <Result
-      className="mt-5 pt-5"
       status="404"
       title="404"
       subTitle={
-        <>
-          <label className="fw-regular text-black">
-            Lo Sentimos, la Página que Buscas no Existe
-          </label>
-        </>
+        <label className="fw-regular text-black">
+          Lo Sentimos, la Página que Buscas no Existe
+        </label>
       }
       extra={
         <>
-          <Button type="primary" onClick={goToHome}>
-            Volver Al Dashboard
-          </Button>
+          <div>
+            <Button type="primary" onClick={goToHome}>
+              Volver
+            </Button>
+          </div>{" "}
           <br />
-          <label
-            className="fw-regular text-black mt-4"
-            style={{ fontSize: "12px" }}
-          >
-            Banco Bambú &reg; <br /> {new Date().getFullYear()} All Rights
-            Reserved.
-          </label>
+          <div>
+            <label
+              className="fw-regular text-black"
+              style={{ fontSize: "12px" }}
+            >
+              &copy;{new Date().getFullYear()} Banco Bambú de El Salvador, S.A
+              de C.V.&reg; <br />
+              Desarrollado por cruzito-x - Todos los Derechos Reservados.
+            </label>
+          </div>
         </>
       }
+      style={{
+        height: "75vh",
+        display: "flex",
+        "flex-direction": "column",
+        "justify-content": "center",
+        "align-items": "center",
+      }}
     />
   );
 };
